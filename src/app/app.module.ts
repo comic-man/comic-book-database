3d1f1002c51cf23ce46d345da70d2b76eb7183dc
David Weidenbenner <44938475+comic-man@users.noreply.github.com>
2021-09-30 19:17:20 -0500

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NewReleasesComponent } from './new-releases/new-releases.component';
+import { CatalogComponent } from './catalog/catalog.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewReleasesComponent,
+    CatalogComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule
