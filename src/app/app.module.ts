4fec4b4412d1bfed841e1f4645dc90ce167c489c
David Weidenbenner <44938475+comic-man@users.noreply.github.com>
2021-10-18 19:29:08 -0500

    jnejcne

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NewReleasesComponent } from './new-releases/new-releases.component';
+import { NewReleasesComponent } from './home/new-releases/new-releases.component';
 import { CatalogComponent } from './catalog/catalog.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { ComicBookInfoService } from './Services/comic-book-info.service';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { Routes, RouterModule } from '@angular/router';
-import { AboutUsComponent } from './about-us/about-us.component';
+import { AboutUsComponent } from './home/about-us/about-us.component';
 
 
 
@@ -20,7 +20,7 @@ import { AboutUsComponent } from './about-us/about-us.component';
     CatalogComponent,
     NavigationComponent,
     HomeComponent,
-    AboutUsComponent
+    AboutUsComponent,
   ],
   imports: [
     BrowserModule,
