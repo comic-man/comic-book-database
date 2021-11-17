61b1e4fda572b1f89af2cac88c5b23d4efdabf85
David Weidenbenner <44938475+comic-man@users.noreply.github.com>
2021-11-16 22:06:02 -0600

    sign-in

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  userService: any;
+  router: any;
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  onSignIn(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email
+    const password = form.value.password
+    this.userService.signIn(email, password).subscribe((data: any) => {
+      this.router.navigate([''])
+      console.log(data)
+    }, (error: any) => {
+      console.log(error)
+    })
 
+    form.reset()
+  }
 }
